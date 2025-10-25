ffa95d433e9d0796919dd3930ad29da4242670b9
Essence Valencia <essencexcodes@gmail.com>
2025-10-25 13:47:15 -0400

    Fixed overlapping CIDR

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,14 +42,21 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "aks-lab"
 
   default_node_pool {
-    name       = "agentpool"
-    node_count = var.agent_count
-    vm_size    = var.agent_size
-    vnet_subnet_id = azurerm_subnet.main.id
+    name            = "agentpool"
+    node_count      = var.agent_count
+    vm_size         = var.agent_size
+    vnet_subnet_id  = azurerm_subnet.main.id
   }
 
   identity {
     type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    service_cidr       = "10.1.0.0/16"
+    dns_service_ip     = "10.1.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
   }
 
   depends_on = [azurerm_container_registry.acr]
