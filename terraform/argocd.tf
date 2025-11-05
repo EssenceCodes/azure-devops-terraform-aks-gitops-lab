9442bc79c93cfbe591e7af6b3f16b303f951035c
Essence Valencia <essencexcodes@gmail.com>
2025-11-05 14:27:13 -0500

    Added Helm

---
+++ terraform/argocd.tf
@@ -0,0 +1,17 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      server = {
+        service = {
+          type = "LoadBalancer" # exposes Argo CD externally
+        }
+      }
+    })
+  ]
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -17,3 +17,12 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  }
+}
